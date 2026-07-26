3fdc7098f7073795caa7795e41cdb73e960a66e8
alvinahada-afk <alvinahada@gmail.com>
2026-07-27 00:31:58 +0700

    Update admin.js

--- admin.js
+++ admin.js
@@ -1,37 +1,28 @@
 const list = document.getElementById("bookingList");
 
 db.collection("booking")
-.onSnapshot((snapshot)=>{
+.get()
+.then((snapshot)=>{
 
-    list.innerHTML = "";
+    list.innerHTML="";
 
     snapshot.forEach((doc)=>{
 
         let data = doc.data();
 
         list.innerHTML += `
-        <div style="
-        background:#111;
-        color:white;
-        padding:20px;
-        margin:15px;
-        border-radius:10px;
-        ">
-
-        <h3>${data.nama}</h3>
-
-        <p>WhatsApp: ${data.nomor}</p>
-        <p>Tanggal: ${data.tanggal}</p>
-        <p>Jam: ${data.jam}</p>
-        <p>Status: ${data.status}</p>
-
-        <a href="https://wa.me/${data.nomor}" target="_blank">
-        Chat WhatsApp
-        </a>
-
+        <div class="card">
+            <h3>${data.nama}</h3>
+            <p>WhatsApp: ${data.nomor}</p>
+            <p>Tanggal: ${data.tanggal}</p>
+            <p>Jam: ${data.jam}</p>
+            <p>Status: ${data.status}</p>
         </div>
         `;
 
     });
 
+})
+.catch((error)=>{
+    list.innerHTML="Error: "+error;
 });
