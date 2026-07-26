f66fcce360876497e533a1e9501c5c7449ed8190
alvinahada-afk <alvinahada@gmail.com>
2026-07-26 22:35:10 +0700

    Add booking function to open WhatsApp link

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
+function booking(){
 
+let pesan =
+"Halo Alvin Barber Studio, saya ingin melakukan booking haircut.";
+
+let nomor =
+"628123456789";
+
+window.open(
+"https://wa.me/"+nomor+"?text="+pesan
+);
+
+}
