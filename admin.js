c0dc988ba2f7d728c5aa56bf726f4a179f28a7f7
alvinahada-afk <alvinahada@gmail.com>
2026-07-27 00:40:00 +0700

    Update admin.js

--- admin.js
+++ admin.js
@@ -4,21 +4,24 @@ db.collection("booking")
 .get()
 .then((snapshot)=>{
 
-    list.innerHTML="";
+    list.innerHTML += `
+<div class="card">
+<h3>${data.nama}</h3>
 
-    snapshot.forEach((doc)=>{
+<p>WhatsApp: ${data.nomor}</p>
 
-        let data = doc.data();
+<p>Tanggal: ${data.tanggal}</p>
 
-        list.innerHTML += `
-        <div class="card">
-            <h3>${data.nama}</h3>
-            <p>WhatsApp: ${data.nomor}</p>
-            <p>Tanggal: ${data.tanggal}</p>
-            <p>Jam: ${data.jam}</p>
-            <p>Status: ${data.status}</p>
-        </div>
-        `;
+<p>Jam: ${data.jam}</p>
+
+<p>Status: ${data.status}</p>
+
+<a href="https://wa.me/6283892513500?text=Booking%20Baru%0A%0ANama:%20${data.nama}%0ANomor:%20${data.nomor}%0ATanggal:%20${data.tanggal}%0AJam:%20${data.jam}" target="_blank">
+Kirim Notifikasi WhatsApp
+</a>
+
+</div>
+`;
 
     });
 
