644c494ed0181ebd3e0c30761513f6e445450d84
alvinahada-afk <alvinahada@gmail.com>
2026-07-26 23:27:35 +0700

    Add Firebase configuration and initialization
    Added Firebase configuration and initialization code.

--- firebase.js
+++ firebase.js
@@ -1,1 +1,14 @@
+// Firebase Config
+const firebaseConfig = {
+  apiKey: "AIzaSyBkpSsKQ1oUkcMaqfz2Im9tVj8Yl9XkpYI",
+  authDomain: "alvin-website-cc21f.firebaseapp.com",
+  projectId: "alvin-website-cc21f",
+  storageBucket: "alvin-website-cc21f.firebasestorage.app",
+  messagingSenderId: "1031795933286",
+  appId: "1:1031795933286:web:7bff285fc6589271f76da7"
+};
 
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+const db = firebase.firestore();
