10d35c424fa28b31d3183b35b728272a3ff1e139
alvinahada-afk <alvinahada@gmail.com>
2026-07-27 12:39:58 +0700

    Update script.js

--- script.js
+++ script.js
@@ -24,7 +24,39 @@ form.addEventListener("submit", (e)=>{
 e.preventDefault();
 
 
+document.getElementById("bookingForm").addEventListener("submit", function(e){
+
+e.preventDefault();
+
+let nama = document.getElementById("nama").value;
+let nomor = document.getElementById("nomor").value;
+let tanggal = document.getElementById("tanggal").value;
+let jam = document.getElementById("jam").value;
+
+
 db.collection("booking").add({
+
+nama: nama,
+nomor: nomor,
+tanggal: tanggal,
+jam: jam,
+status: "Menunggu"
+
+})
+.then(()=>{
+
+alert("Booking berhasil dikirim!");
+
+document.getElementById("bookingForm").reset();
+
+})
+.catch((error)=>{
+
+alert("Gagal: " + error.message);
+
+});
+
+});
 
 nama: document.getElementById("nama").value,
 
