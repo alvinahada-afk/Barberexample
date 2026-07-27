c875f6ed3867c8ff0e8b7bcebc35575e1d7273a3
alvinahada-afk <alvinahada@gmail.com>
2026-07-27 16:10:26 +0700

    Update script.js

--- script.js
+++ script.js
@@ -38,11 +38,71 @@ let tanggal = document.getElementById("tanggal").value;
 let jam = document.getElementById("jam").value;
 
 
-
 db.collection("booking")
 .where("tanggal","==",tanggal)
 .where("jam","==",jam)
 .get()
+.then((snapshot)=>{
+
+
+if(!snapshot.empty){
+
+alert("❌ Jam tersebut sudah dibooking. Silakan pilih jam lain.");
+return;
+
+}
+
+
+db.collection("booking").add({
+
+nama:nama,
+nomor:nomor,
+tanggal:tanggal,
+jam:jam,
+status:"Pending"
+
+})
+.then(()=>{
+
+
+let pesan =
+"🔔 Booking Baru Alvin Barber Studio\n\n"+
+"Nama: "+nama+"\n"+
+"WhatsApp: "+nomor+"\n"+
+"Tanggal: "+tanggal+"\n"+
+"Jam: "+jam+"\n"+
+"Status: Pending";
+
+
+let nomorPemilik="6283892513500";
+
+
+window.open(
+"https://wa.me/"+nomorPemilik+"?text="+encodeURIComponent(pesan),
+"_blank"
+);
+
+
+alert("Booking berhasil dikirim!");
+
+form.reset();
+
+
+})
+.catch((error)=>{
+
+alert("Gagal: "+error.message);
+
+});
+
+
+})
+.catch((error)=>{
+
+alert("Error cek booking: "+error.message);
+
+});
+
 .then((snapshot)=>{
 
 
