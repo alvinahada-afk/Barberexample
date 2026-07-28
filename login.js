a1d9c32f90a8026cd6a38adb5f06f826320abb68
alvinahada-afk <alvinahada@gmail.com>
2026-07-28 11:57:18 +0700

    Add login function with authentication handling
    Implement login functionality with email and password authentication.

--- login.js
+++ login.js
@@ -1,1 +1,45 @@
+function login(){
 
+
+let email = document.getElementById("email").value;
+
+let password = document.getElementById("password").value;
+
+
+auth.signInWithEmailAndPassword(email,password)
+
+.then(()=>{
+
+
+window.location.href="admin.html";
+
+
+})
+
+
+.catch((error)=>{
+
+
+document.getElementById("pesan").innerHTML =
+"Login gagal: " + error.message;
+
+
+});
+
+
+}
+
+
+// cek kalau sudah login
+
+auth.onAuthStateChanged((user)=>{
+
+
+if(user){
+
+window.location.href="admin.html";
+
+}
+
+
+});
