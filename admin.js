b685705fc7786f6ecab9b4bde5b465f8aa754c24
alvinahada-afk <alvinahada@gmail.com>
2026-07-28 14:29:00 +0700

    Update admin.js

--- admin.js
+++ admin.js
@@ -1,3 +1,6 @@
+const db = firebase.firestore();
+
+
 firebase.auth().onAuthStateChanged(function(user){
 
 if(!user){
@@ -10,10 +13,62 @@ window.location.href="login.html";
 
 
 
+db.collection("booking").onSnapshot(function(snapshot){
+
+let total = snapshot.size;
+
+document.getElementById("totalPesanan").innerHTML = total;
+
+
+let baru = 0;
+let html = "";
+
+
+snapshot.forEach(function(doc){
+
+
+let data = doc.data();
+
+
+if(data.status == "Diterima" || data.status == "Baru"){
+baru++;
+}
+
+
+html += `
+<tr>
+<td>${data.nama || "-"}</td>
+<td>${data.layanan || "-"}</td>
+<td>${data.status || "-"}</td>
+</tr>
+`;
+
+
+});
+
+
+document.getElementById("pesananBaru").innerHTML = baru;
+
+
+document.querySelector("table").innerHTML = `
+
+<tr>
+<th>Nama</th>
+<th>Layanan</th>
+<th>Status</th>
+</tr>
+
+${html}
+
+`;
+
+
+});
+
+
 function logout(){
 
 firebase.auth().signOut()
-
 .then(function(){
 
 window.location.href="login.html";
