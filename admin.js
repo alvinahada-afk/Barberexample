aba701385b352ed4df3e3cb5ef6df0ef91955ce7
alvinahada-afk <alvinahada@gmail.com>
2026-07-28 14:19:43 +0700

    Add authentication check and logout function

--- admin.js
+++ admin.js
@@ -1,1 +1,23 @@
+firebase.auth().onAuthStateChanged(function(user){
 
+if(!user){
+
+window.location.href="login.html";
+
+}
+
+});
+
+
+
+function logout(){
+
+firebase.auth().signOut()
+
+.then(function(){
+
+window.location.href="login.html";
+
+});
+
+}
