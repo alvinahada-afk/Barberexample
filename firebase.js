d9e56057c3f3e799fe644164388a51202855bb39
alvinahada-afk <alvinahada@gmail.com>
2026-07-28 12:08:55 +0700

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,16 +1,10 @@
-// Firebase Config
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "AIzaSyBkpSsKQ1oUkcMaqfz2Im9tVj8Yl9XkpYI",
   authDomain: "alvin-website-cc21f.firebaseapp.com",
   projectId: "alvin-website-cc21f",
   storageBucket: "alvin-website-cc21f.firebasestorage.app",
   messagingSenderId: "1031795933286",
-  appId: "1:1031795933286:web:7bff285fc6589271f76da7"
+  appId: "1:1031795933286:web:7bff285fc6589271f76da7",
+  measurementId: "G-26ZLF4L1RK"
 };
-
-// Initialize Firebase
-
-firebase.initializeApp(firebaseConfig);
-
-const auth = firebase.auth();
-const db = firebase.firestore();
