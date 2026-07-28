15e96608b2dd6a790a0ba0cf1248e9857cb11d9f
alvinahada-afk <alvinahada@gmail.com>
2026-07-28 23:59:27 +0700

    Implement error handling for settings retrieval
    Added error handling for fetching settings data.

--- website.js
+++ website.js
@@ -1,25 +1,24 @@
 const settingRef = db.collection("settings").doc("toko");
 
-
 settingRef.get().then((doc)=>{
 
 if(doc.exists){
 
 let data = doc.data();
 
-
 document.getElementById("namaToko").innerHTML =
 data.namaToko;
-
 
 document.getElementById("deskripsiToko").innerHTML =
 data.deskripsi;
 
-
 document.getElementById("jamBuka").innerHTML =
 "Jam Buka : " + data.jamBuka;
 
-
 }
+
+}).catch((error)=>{
+
+console.log(error);
 
 });
