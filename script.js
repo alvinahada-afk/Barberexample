f226de3e633fa124b3a2fc7ed25f9a868393f09f
alvinahada-afk <alvinahada@gmail.com>
2026-07-29 16:58:08 +0700

    Remove alert statements from script.js
    Remove alert statements for Firebase responses.

--- script.js
+++ script.js
@@ -3,19 +3,15 @@ db.collection("settings")
 .get()
 .then((doc)=>{
 
-alert("Firebase terbaca");
 
 if(doc.exists){
 
 let data = doc.data();
 
-alert(JSON.stringify(data));
 
 }
 
 })
 .catch((error)=>{
-
-alert(error.message);
 
 });
