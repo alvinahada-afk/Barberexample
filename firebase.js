060c897a646d0e741685b57c2fa634d4d92847d1
alvinahada-afk <alvinahada@gmail.com>
2026-07-29 13:03:48 +0700

    Refactor firebase configuration and initialize Firestore

--- firebase.js
+++ firebase.js
@@ -1,13 +1,13 @@
 const firebaseConfig = {
-  apiKey: "AIzaSyBkpSsKQ1oUkcMaqfz2Im9tVj8Yl9XkpYI",
-  authDomain: "alvin-website-cc21f.firebaseapp.com",
-  projectId: "alvin-website-cc21f",
-  storageBucket: "alvin-website-cc21f.firebasestorage.app",
-  messagingSenderId: "1031795933286",
-  appId: "1:1031795933286:web:7bff285fc6589271f76da7"
+apiKey: "AIzaSyBkpSsKQ1oUkcMaqfz2Im9tVj8Yl9XkpYI",
+authDomain: "alvin-website-cc21f.firebaseapp.com",
+projectId: "alvin-website-cc21f",
+storageBucket: "alvin-website-cc21f.firebasestorage.app",
+messagingSenderId: "1031795933286",
+appId: "1:1031795933286:web:7bff285fc6589271f76da7"
 };
 
 
 firebase.initializeApp(firebaseConfig);
 
-window.db = firebase.firestore();
+const db = firebase.firestore();
