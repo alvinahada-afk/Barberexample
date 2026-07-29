94a0430ac308521b858280830c39ce790942ce4c
alvinahada-afk <alvinahada@gmail.com>
2026-07-29 18:19:35 +0700

    Update services.js

--- services.js
+++ services.js
@@ -1,1 +1,36 @@
-alert("SERVICES TERBACA");
+import { db } from "./firebase.js";
+import { collection, getDocs } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
+
+const serviceList = document.getElementById("service-list");
+
+async function loadServices(){
+
+    try {
+
+        const snapshot = await getDocs(collection(db,"services"));
+
+        serviceList.innerHTML = "";
+
+        snapshot.forEach((doc)=>{
+
+            const data = doc.data();
+
+            serviceList.innerHTML += `
+            <div class="service-card">
+                <h3>${data.nama}</h3>
+                <p>Rp ${data.harga}</p>
+                <p>${data.deskripsi}</p>
+            </div>
+            `;
+
+        });
+
+    } catch(error){
+
+        console.log(error);
+
+    }
+
+}
+
+loadServices();
