d418b56b31f72f41487c515cc9962f91fcb00bd9
alvinahada-afk <alvinahada@gmail.com>
2026-07-29 12:35:06 +0700

    Refactor loadServices function to use querySnapshot

--- services.js
+++ services.js
@@ -1,56 +1,35 @@
-import { db } from "./firebase.js";
-import { collection, getDocs } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
-
-
 const serviceList = document.getElementById("serviceList");
-
 
 async function loadServices(){
 
-try{
+    const querySnapshot = await db.collection("services").get();
 
-const snapshot = await getDocs(collection(db,"services"));
+    serviceList.innerHTML = "";
 
+    querySnapshot.forEach((doc)=>{
 
-serviceList.innerHTML="";
+        let data = doc.data();
 
+        serviceList.innerHTML += `
+        
+        <div class="card">
 
-snapshot.forEach((doc)=>{
+            <h3>${data.nama}</h3>
 
+            <p>
+            Rp${Number(data.harga).toLocaleString("id-ID")}
+            </p>
 
-let data = doc.data();
+            <p>
+            ${data.deskripsi}
+            </p>
 
+        </div>
 
-serviceList.innerHTML += `
+        `;
 
-<div class="card">
-
-<h3>${data.nama}</h3>
-
-<p>
-Rp ${data.harga.toLocaleString("id-ID")}
-</p>
-
-<p>
-${data.deskripsi}
-</p>
-
-</div>
-
-`;
-
-
-});
-
-
-}catch(error){
-
-console.log(error);
+    });
 
 }
-
-
-}
-
 
 loadServices();
