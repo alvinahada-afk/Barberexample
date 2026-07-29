8c9ea6994a88233e890369b1ed9c1abf25bbda20
alvinahada-afk <alvinahada@gmail.com>
2026-07-29 12:28:13 +0700

    Refactor Firebase initialization and Firestore setup

--- firebase.js
+++ firebase.js
@@ -1,14 +1,32 @@
 // Firebase Config
+
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
+
+import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
+
+
 const firebaseConfig = {
-  apiKey: "AIzaSyBkpSsKQ1oUkcMaqfz2Im9tVj8Yl9XkpYI",
-  authDomain: "alvin-website-cc21f.firebaseapp.com",
-  projectId: "alvin-website-cc21f",
-  storageBucket: "alvin-website-cc21f.firebasestorage.app",
-  messagingSenderId: "1031795933286",
-  appId: "1:1031795933286:web:7bff285fc6589271f76da7"
+
+apiKey: "AIzaSyBkpSsKQ1oUkcMaqfz2Im9tVj8Yl9XkpYI",
+
+authDomain: "alvin-website-cc21f.firebaseapp.com",
+
+projectId: "alvin-website-cc21f",
+
+storageBucket: "alvin-website-cc21f.firebasestorage.app",
+
+messagingSenderId: "1031795933286",
+
+appId: "1:1031795933286:web:7bff285fc6589271f76da7"
+
 };
 
-// Initialize Firebase
-firebase.initializeApp(firebaseConfig);
 
-const db = firebase.firestore();
+// Initialize Firebase
+
+const app = initializeApp(firebaseConfig);
+
+
+// Firestore
+
+export const db = getFirestore(app);
