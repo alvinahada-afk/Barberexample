7b979399df8f1d27ecec06346e7dcab72dea22fc
alvinahada-afk <alvinahada@gmail.com>
2026-08-02 18:14:32 +0700

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -4,12 +4,13 @@ import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.0/firebas
 import { getStorage } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-storage.js";
 
 const firebaseConfig = {
-  apiKey: "ISI DISINI",
-  authDomain: "ISI DISINI",
-  projectId: "ISI DISINI",
-  storageBucket: "ISI DISINI",
-  messagingSenderId: "ISI DISINI",
-  appId: "ISI DISINI"
+  apiKey: "AIzaSyBdYr7LfwLAip0Tcxg5cYI0E0gJ9sPuo5k",
+  authDomain: "barberpro-belajar.firebaseapp.com",
+  projectId: "barberpro-belajar",
+  storageBucket: "barberpro-belajar.firebasestorage.app",
+  messagingSenderId: "978171873500",
+  appId: "1:978171873500:web:497d4c9d90fbc341948c19"
+  measurementId: "G-56FYNZHB7J"
 };
 
 const app = initializeApp(firebaseConfig);
