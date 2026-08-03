f7736616aa37a711188422c39b37c4cb2fa45d0c
alvinahada-afk <alvinahada@gmail.com>
2026-08-03 21:36:41 +0700

    Replace Firebase config values with placeholders
    Updated Firebase configuration with placeholders for sensitive data.

--- firebase.js
+++ firebase.js
@@ -1,19 +1,16 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-app.js";
 import { getAuth } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-auth.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-firestore.js";
-import { getStorage } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-storage.js";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBdYr7LfwLAip0Tcxg5cYI0E0gJ9sPuo5k",
-  authDomain: "barberpro-belajar.firebaseapp.com",
-  projectId: "barberpro-belajar",
-  storageBucket: "barberpro-belajar.firebasestorage.app",
-  messagingSenderId: "978171873500",
-  appId: "1:978171873500:web:497d4c9d90fbc341948c19"
-  measurementId: "G-56FYNZHB7J"
+  apiKey: "GANTI_DENGAN_API_KEY_KAMU",
+  authDomain: "GANTI_DENGAN_AUTH_DOMAIN",
+  projectId: "GANTI_DENGAN_PROJECT_ID",
+  storageBucket: "GANTI_DENGAN_STORAGE_BUCKET",
+  messagingSenderId: "GANTI_DENGAN_SENDER_ID",
+  appId: "GANTI_DENGAN_APP_ID"
 };
 
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
 export const db = getFirestore(app);
-export const storage = getStorage(app);
